b4444ed3f731232bf566bd0842e6787040b4903f
amitkr7 <amit840901@gmail.com>
2024-06-01 23:03:59 +0530

    Same Frequency problem

--- sameFrequency.js
+++ sameFrequency.js
@@ -1,24 +1,44 @@
-function sameFrequency(num1, num2){
-    let strNum1 = num1.toString();
-    let strNum2 = num2.toString();
-    if(strNum1.length !== strNum2.length) return false;
-    
-    let countNum1 = {};
-    let countNum2 = {};
-    
-    for(let i = 0; i < strNum1.length; i++){
-      countNum1[strNum1[i]] = (countNum1[strNum1[i]] || 0) + 1
-    }
-    
-    for(let j = 0; j < strNum1.length; j++){
-      countNum2[strNum2[j]] = (countNum2[strNum2[j]] || 0) + 1
-    }
-    
-    for(let key in countNum1){
-      if(countNum1[key] !== countNum2[key]) return false;
-    }
-   
-    return true;
+function sameFrequency(num1, num2) {
+  let strNum1 = num1.toString();
+  let strNum2 = num2.toString();
+  if (strNum1.length !== strNum2.length) return false;
+
+  let countNum1 = {};
+  let countNum2 = {};
+
+  for (let i = 0; i < strNum1.length; i++) {
+    countNum1[strNum1[i]] = (countNum1[strNum1[i]] || 0) + 1;
   }
 
- console.log( sameFrequency(123,321))
+  for (let j = 0; j < strNum1.length; j++) {
+    countNum2[strNum2[j]] = (countNum2[strNum2[j]] || 0) + 1;
+  }
+  for (let key in countNum1) {
+    if (countNum1[key] !== countNum2[key]) return false;
+  }
+
+  return true;
+}
+
+const sameFrequencyArr = (arr1, arr2) => {
+  if (arr1.length !== arr2.length) return false;
+
+  let counter1 = {};
+  let counter2 = {};
+
+  for (let val of arr1) {
+    counter1[val] = (counter1[val] || 0) + 1;
+  }
+
+  for (let val of arr2) {
+    counter2[val] = (counter2[val] || 0) + 1;
+  }
+
+  for (let key in counter1) {
+    if (!(key ** 2 in counter2)) return false;
+    if (counter2[key ** 2] !== counter1[key]) return false;
+  }
+  return true;
+};
+
+console.log(sameFrequencyArr([1, 2, 1], [4, 2, 1]));
