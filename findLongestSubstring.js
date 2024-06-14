e884281159bdcdf169d0c3446690b1c6a6c31689
Amit <amit840901@gmail.com>
2024-06-14 21:47:51 +0530

    Findlongest Substring 2

--- findLongestSubstring.js
+++ findLongestSubstring.js
@@ -22,10 +22,36 @@ const findLongestSubstring = (str) => {
   return longestSub;
 };
 
-findLongestSubstring(''); // 0
-findLongestSubstring('rithmschool'); // 7
-findLongestSubstring('thisisawesome'); // 6
-findLongestSubstring('thecatinthehat'); // 7
-findLongestSubstring('bbbbbb'); // 1
-findLongestSubstring('longestsubstring'); // 8
-findLongestSubstring('thisishowwedoit'); // 6
+// findLongestSubstring(''); // 0
+// findLongestSubstring('rithmschool'); // 7
+// findLongestSubstring('thisisawesome'); // 6
+// findLongestSubstring('thecatinthehat'); // 7
+// findLongestSubstring('bbbbbb'); // 1
+// findLongestSubstring('longestsubstring'); // 8
+// findLongestSubstring('thisishowwedoit'); // 6
+
+const findLongestSubstring2 = (str) => {
+  let start = 0;
+  let longest = 0;
+  let seen = {};
+
+  for (let i = 0; i < str.length; i++) {
+    let char = str[i];
+
+    if (seen[char]) {
+      start = Math.max(start, seen[char]);
+    }
+    longest = Math.max(longest, i - start + 1);
+    seen[char] = i + 1;
+  }
+  console.log('longest', longest);
+  return longest;
+};
+
+findLongestSubstring2(''); // 0
+findLongestSubstring2('rithmschool'); // 7
+findLongestSubstring2('thisisawesome'); // 6
+findLongestSubstring2('thecatinthehat'); // 7
+findLongestSubstring2('bbbbbb'); // 1
+findLongestSubstring2('longestsubstring'); // 8
+findLongestSubstring2('thisishowwedoit'); // 6
